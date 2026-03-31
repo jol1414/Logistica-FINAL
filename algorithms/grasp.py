f25fb478bc75049a27358cc04b6005244226a17c
Joel Ezequiel Vicente Alonso <124356990+jol1414@users.noreply.github.com>
2026-03-31 18:58:21 +0200

    Add files via upload

---
+++ algorithms/grasp.py
@@ -0,0 +1,15 @@
+from constructives import cgrasp
+from localsearch import lsbestimp
+
+def execute(inst, iters, alpha):
+    best = None
+    for i in range(iters):
+        print("Iter "+str(i+1)+": ", end="")
+        sol = cgrasp.construct(inst, alpha)
+        print("C -> "+str(round(sol['of'], 2)), end=", ")
+        lsbestimp.improve(sol)
+        print("LS -> "+str(round(sol['of'], 2)))
+        # This way it wont just return the best
+        # if best is None or best['of'] < sol['of']:
+        #     best = sol
+    return sol
